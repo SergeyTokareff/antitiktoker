1d1c845c9a9f37035e652071051bc6d3b78bb375
SergeyTokareff <sergey.v.tokareff@gmail.com>
2021-11-13 16:08:34 +0200

    Add base bot logic

---
+++ config.py
@@ -0,0 +1,2 @@
+BOT_TOKEN = '2106521845:AAFhtQMazZESLSD0sXoMR0rMkTQAeR1sUEo'
+APP_URL = 'https://antitiktoker.herokuapp.com/' + BOT_TOKEN
---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import telebot
+import logging
+from config import *
+from flask import Flask, request
+
+bot = telebot.TeleBot(BOT_TOKEN)
+server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
+
+
+@bot.message_handler(commands=["start"])
+def get_text_messages(message):
+    res = message.text.lower().find('tiktok')
+
+    if res > -1:
+        bot.delete_message(message.chat.id, message.message_id)
+
+
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
